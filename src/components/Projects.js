4831ab3746d2fe003fa16a2a78f9c203db1b2df8
nachosala89 <nachosala89@gmail.com>
2022-05-23 10:55:21 -0300

    Add favicon

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,8 +3,8 @@ import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
   return (
-    <section>
-      <h2>My recent works</h2>
+    <section className="green-bcg p-10 rounded-3xl">
+      <h2 className="poppins text-4xl">My recent works</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         <ProjectCard />
         <ProjectCard />
